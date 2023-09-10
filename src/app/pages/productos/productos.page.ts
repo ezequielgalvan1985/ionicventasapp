8700894c868a7f7a5ee2020e23d52e30afad63f5
ezequiel <ezgatrabajo@gmail.com>
2023-09-10 20:31:59 -0300

    publicar producto

--- src/app/pages/productos/productos.page.ts
+++ src/app/pages/productos/productos.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Categoria } from 'src/app/models/categoria';
+import { Marca } from 'src/app/models/marca';
+import { Producto } from 'src/app/models/producto';
+import { ProductoService } from 'src/app/services/producto.service';
 
 @Component({
   selector: 'app-productos',
@@ -6,10 +11,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./productos.page.scss'],
 })
 export class ProductosPage implements OnInit {
+  productoForm={}as FormGroup;
+  entityProducto = {} as Producto;
+  categoriasList = [] as Categoria[];
+  marcasList = [] as Marca[];
 
-  constructor() { }
+  isToastOpen = false;
+  mensajeToast= '';
+
+  isAlertOpen = false;
+  public alertButtons = ['OK'];
+  messageAlert = '';
+
+
+  productoId = 0;
+
+  constructor(private productoService:ProductoService) { 
+
+  }
+
 
   ngOnInit() {
   }
 
+  fnLoadProductosByEmpresa(empresaId:number){
+    this.productoService.
+  }
+
+
+  setOpenToast(isOpen: boolean) {
+    this.isToastOpen = isOpen;
+  }
+
+  
+  setOpenAlert(isOpen: boolean) {
+    this.isAlertOpen = isOpen;
+  }
+  
 }
